7421afe6cef03e06443d1503f183db8d54463e04
leoeliasr <80182708+leoeliasr@users.noreply.github.com>
2024-04-09 14:27:58 -0300

    Adiciona classes A, B e C ao controle de versão

--- A.java
+++ A.java
@@ -19,32 +19,4 @@ public class A {
     }
 }
 
-// Nova classe C
-public class C {
-    private String C1;
-    private int C2;
 
-    public String getC1() {
-        return C1;
-    }
-
-    public void setC1(String c1) {
-        C1 = c1;
-    }
-
-    public int getC2() {
-        return C2;
-    }
-
-    public void setC2(int c2) {
-        C2 = c2;
-    }
-
-    public void MC1() {
-        System.out.println("Método MC1");
-    }
-
-    public void MC2() {
-        System.out.println("Método MC2");
-    }
-}
