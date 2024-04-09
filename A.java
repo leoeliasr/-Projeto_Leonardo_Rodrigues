e5fb3049d58f18eac73ee2191c340b17ffed6364
leoeliasr <80182708+leoeliasr@users.noreply.github.com>
2024-04-09 13:46:25 -0300

    Alteração a classe A partir do clone

--- A.java
+++ A.java
@@ -1,23 +1,9 @@
-// Classe A
+// Classe A atualizada
 public class A {
     private int A1;
     private float A2;
 
-    public int getA1() {
-        return A1;
-    }
-
-    public void setA1(int a1) {
-        A1 = a1;
-    }
-
-    public float getA2() {
-        return A2;
-    }
-
-    public void setA2(float a2) {
-        A2 = a2;
-    }
+    // Getters e setters omitidos para brevidade
 
     public void MA1() {
         System.out.println("Método MA1");
@@ -27,12 +13,38 @@ public class A {
         System.out.println("Método MA2");
     }
 
-    // Método main adicionado
-    public static void main(String[] args) {
-        A objetoA = new A();
-        objetoA.setA1(10); // Exemplo de uso do setter
-        objetoA.setA2(20.0f); // Exemplo de uso do setter
-        objetoA.MA1();
-        objetoA.MA2();
+    // Novo método MA3
+    public void MA3() {
+        System.out.println("Alteração a classe A partir do clone");
+    }
+}
+
+// Nova classe C
+public class C {
+    private String C1;
+    private int C2;
+
+    public String getC1() {
+        return C1;
+    }
+
+    public void setC1(String c1) {
+        C1 = c1;
+    }
+
+    public int getC2() {
+        return C2;
+    }
+
+    public void setC2(int c2) {
+        C2 = c2;
+    }
+
+    public void MC1() {
+        System.out.println("Método MC1");
+    }
+
+    public void MC2() {
+        System.out.println("Método MC2");
     }
 }
